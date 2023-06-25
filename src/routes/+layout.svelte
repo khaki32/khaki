55673090649f93d45be69526e14afc7255a5e674
khaki <khakiaunt32@gmail.com>
2023-06-25 20:16:58 +0200

    Add Navbar

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,28 @@
+<script>
+  import "../app.css";
+
+  import Navbar from "$lib/Navbar.svelte";
+</script>
+
+<Navbar />
+
+<main>
+  <div>
+    <slot></slot>
+  </div>
+</main>
+
+<style>
+  main {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  div {
+    display: flex;
+    flex-direction: column;
+    width: 800px;
+    padding: 1rem;
+  }
+</style>
