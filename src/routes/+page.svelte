77f8f146ebcbc26d22434a56eab0dd47c12a0f6d
khaki <khakiaunt32@gmail.com>
2023-06-26 00:20:06 +0200

    Add favorite movies

---
+++ src/routes/+page.svelte
@@ -0,0 +1,34 @@
+<section>
+  <h2>favorite movies</h2>
+  <div>
+    <a href="/marriage-story">
+      <img src="/marriage-story.jpg" alt="marriage story">
+    </a>
+    <a href="/before-sunrise.jpg">
+      <img src="/before-sunrise.jpg" alt="before sunrise">
+    </a>
+    <a href="/before-sunset">
+      <img src="/before-sunset.jpg" alt="before sunset">
+    </a>
+    <a href="/good-will-hunting">
+      <img src="/good-will-hunting.jpg" alt="good will hunting">
+    </a>
+  </div>
+</section>
+
+<style>
+  div {
+    display: flex;
+    gap: 10px;
+  }
+
+  img {
+    border: 3px solid var(--accent-color);
+    width: calc((var(--page-width) - 50px) / 4);
+  }
+
+  h2 {
+    text-transform: uppercase;
+    color: var(--accent-color);
+  }
+</style>
