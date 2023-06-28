c27d78e76bac046747c12a857970fb6e88f334d8
khaki <khakiaunt32@gmail.com>
2023-06-29 00:17:52 +0200

    Optimize images even more

---
+++ src/routes/before-sunrise/+page.svelte
@@ -0,0 +1,46 @@
+<script>
+  import banner from "$lib/images/banner/before-sunrise.webp";
+</script>
+
+<div class="wrapper">
+  <img src={banner} alt="Before Sunrise banner" />
+</div>
+
+<header>
+  <h1>Before Sunrise</h1>
+  <h3>Directed by Richard Linklater</h3>
+</header>
+
+<h4>
+  Jesse and Céline meet on a train and spend a magical evening exploring Vienna,
+  while sharing deep conversations and a fleeting but profound connection.
+</h4>
+
+<p>
+  The film is a masterclass in dialogue-driven storytelling, with Hawke and
+  Delpy delivering remarkable performances that feel natural, authentic, and
+  incredibly captivating. Their chemistry is palpable, drawing viewers into
+  their whirlwind romance and making us feel like participants in their intimate
+  conversations. The dialogue itself is intelligent, heartfelt, and brimming
+  with philosophical musings on life, love, and the human condition. It is a
+  testament to the film's brilliance that it manages to hold our attention and
+  evoke such genuine emotions despite its simple premise.
+</p>
+
+<style>
+  .wrapper {
+    width: 100%;
+    height: 0;
+    padding-top: 56%;
+    position: relative;
+    background-color: var(--accent-color);
+  }
+
+  img {
+    width: 100%;
+    position: absolute;
+    top: 0;
+    left: 0;
+    border: 3px solid var(--accent-color);
+  }
+</style>
---
+++ src/routes/marriage-story/+page.svelte
@@ -0,0 +1,47 @@
+<script>
+  import banner from "$lib/images/banner/marriage-story.webp";
+</script>
+
+<div class="wrapper">
+  <img src={banner} alt="Marriage Story banner" />
+</div>
+
+<header>
+  <h1>Marriage Story</h1>
+  <h3>Directed by Noah Baumbach</h3>
+</header>
+
+<h4>
+  A stage director and an actress struggle through a grueling, coast-to-coast
+  divorce that pushes them to their personal extremes.
+</h4>
+
+<p>
+  The story follows Charlie and Nicole, a couple going through a heartbreaking
+  divorce. As they navigate the complexities of separation, the film explores
+  the raw emotions, challenges, and compromises that arise in the process.
+  Through exceptional performances by Adam Driver and Scarlett Johansson,
+  "Marriage Story" presents a poignant and realistic portrayal of the
+  complexities of love, the breakdown of a marriage, and the impact it has on
+  individuals and their families. It offers a thought-provoking and emotionally
+  resonant examination of the human condition and the fragility of
+  relationships.
+</p>
+
+<style>
+  .wrapper {
+    width: 100%;
+    height: 0;
+    padding-top: 56%;
+    position: relative;
+    background-color: var(--accent-color);
+  }
+
+  img {
+    width: 100%;
+    position: absolute;
+    top: 0;
+    left: 0;
+    border: 3px solid var(--accent-color);
+  }
+</style>
