1a77fd1d80ee1d1c1a26c8288c102f77abba0ea1
khaki <khakiaunt32@gmail.com>
2023-07-03 23:02:43 +0200

    Add logo

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -1,7 +1,16 @@
+<script>
+  import logo from "$lib/images/logo.png";
+</script>
+
 <nav>
-  <a href="/">
-    <h1>khaki</h1>
-  </a>
+  <header>
+    <a href="/">
+      <img src={logo} alt="Jesse Pinkman, from Breaking Bad, sitting in his car">
+    </a>
+    <a href="/">
+      <h1>khaki</h1>
+    </a>
+  </header>
   <section>
     <a href="https://github.com/khaki32/khaki987">
       <h2>github</h2>
@@ -21,5 +30,15 @@
     display: flex;
     align-items: center;
     gap: 5px;
+  }
+
+  header {
+    display: flex;
+    gap: 10px;
+  }
+
+  img {
+    border: 3px solid #3f3b3b;
+    height: 50px;
   }
 </style>
